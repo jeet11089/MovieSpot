90d600b34fd0d1a74ff9ce610e7a001840fb41b2
Ajeet Gupta <ajeetgupta@Ajeets-MacBook-Air.local>
2018-03-09 01:15:12 +0530

    Add new pages

--- MovieSpotApp/src/app/app.module.ts
+++ MovieSpotApp/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AsideComponent } from './shared/aside/aside.component';
+import { SectionComponent } from './shared/section/section.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    SectionComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
